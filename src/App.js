2519cb908ab9fbad928ef70eb375f8864f5403d2
vs0uz4 <vitor.rodrigues@gmail.com>
2018-11-11 19:12:36 -0200

    Added Home Layout, Format Code with Prettier and Remove Unecessary Comments

--- src/App.js
+++ src/App.js
@@ -1,28 +1,90 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+          <div className="container">
+            <div className="navbar-header page-scroll">
+              <a className="navbar-brand page-scroll" href="#page-top">
+                <img src="images/icon.png" height="40" alt="icon" />
+              </a>
+            </div>
+
+            <div className="collapse navbar-collapse navbar-ex1-collapse">
+              <ul className="nav navbar-nav">
+                <li>
+                  <a href="">Menu item</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <section id="intro" className="intro-section">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <h1>
+                  <img src="images/logo.png" alt="logo" />
+                </h1>
+                <p>Nunca mais esqueça uma série que você assistiu ou que alguém lhe indicou.</p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="services" className="services-section">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <h1>Para assistir</h1>
+                <div id="series" className="row list-group">
+                  <div className="item  col-xs-4 col-lg-4">
+                    <div className="thumbnail">
+                      <img className="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
+                      <div className="caption">
+                        <h4 className="group inner list-group-item-heading">How I met your mother</h4>
+                        <div className="row">
+                          <div className="col-xs-12 col-md-6">
+                            <p className="lead">AÇÃO</p>
+                          </div>
+                          <div className="col-xs-12 col-md-6">
+                            <a className="btn btn-success" href="">
+                              Gerenciar
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="item  col-xs-4 col-lg-4">
+                    <div className="thumbnail">
+                      <img className="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
+                      <div className="caption">
+                        <h4 className="group inner list-group-item-heading">How I met your mother</h4>
+                        <div className="row">
+                          <div className="col-xs-12 col-md-6">
+                            <p className="lead">AÇÃO</p>
+                          </div>
+                          <div className="col-xs-12 col-md-6">
+                            <a className="btn btn-success" href="http://www.jquery2dotnet.com">
+                              Gerenciar
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
