0eef43c27930293c77e911ad2cef34e9a8564f1c
vs0uz4 <vitor.rodrigues@gmail.com>
2018-11-13 19:24:06 -0200

    Refactored loadGenres and Implemented saveSerie and loadSeriesByGenre

--- src/Api.js
+++ src/Api.js
@@ -4,8 +4,14 @@ const api = axios.create({
   baseURL: 'http://localhost:3001/'
 })
 
+export const loadGenres = () => api.get('genres')
+export const saveSerie = newSerie => api.post('series', newSerie)
+export const loadSeriesByGenre = genre => api.get(`series?genre=${genre}`)
+
 const apis = {
-  loadGenres: () => api.get('genres')
+  loadGenres: loadGenres,
+  saveSerie: saveSerie,
+  loadSeriesByGenre: loadSeriesByGenre
 }
 
 export default apis
