9abb8edd160e56b79266d9cfe1113543d71b2779
vs0uz4 <vitor.rodrigues@gmail.com>
2018-11-13 23:11:53 -0200

    Implemented methods loadSerieById and updateSerie in API.js

--- src/Api.js
+++ src/Api.js
@@ -5,15 +5,19 @@ const api = axios.create({
 })
 
 export const loadGenres = () => api.get('genres')
-export const saveSerie = newSerie => api.post('series', newSerie)
 export const loadSeriesByGenre = genre => api.get(`series?genre=${genre}`)
+export const loadSerieById = id => api.get(`series/${id}`)
+export const saveSerie = newSerie => api.post('series', newSerie)
+export const updateSerie = updatedSerie => api.put(`series/${updatedSerie.id}`, updatedSerie)
 export const deleteSerie = id => api.delete(`series/${id}`)
 
 const apis = {
-  loadGenres: loadGenres,
-  loadSeriesByGenre: loadSeriesByGenre,
-  saveSerie: saveSerie,
-  deleteSerie: deleteSerie
+  loadGenres,
+  loadSeriesByGenre,
+  loadSerieById,
+  saveSerie,
+  updateSerie,
+  deleteSerie
 }
 
 export default apis
